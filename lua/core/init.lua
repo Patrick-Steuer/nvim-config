fccc50e5a9d05f30abd7840880527d527de30dc9
Patrick Steuer <realpatricksteuer@gmail.com>
2024-05-01 22:34:00 +0200

    Initial commit

---
+++ lua/core/init.lua
@@ -0,0 +1,9 @@
+require("core.remap")
+require("core.lazy")
+
+vim.wo.number = true
+vim.api.nvim_set_option("clipboard", "unnamed")
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.bo.softtabstop = 4
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,38 @@
+return {
+	{
+		"williamboman/mason.nvim",
+
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" , "pylsp" , "tsserver" , "cssls", "html", "clangd", "jdtls", "gradle_ls"}
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({capabilities = capabilities})
+			lspconfig.pylsp.setup({capabilities = capabilities})
+			lspconfig.tsserver.setup({capabilities = capabilities})
+			lspconfig.cssls.setup({capabilities = capabilities})
+			lspconfig.html.setup({capabilities = capabilities})
+			lspconfig.clangd.setup({capabilities = capabilities})
+			lspconfig.jdtls.setup({capabilities = capabilities})
+			lspconfig.gradle_ls.setup({capabilities = capabilities})
+
+			vim.keymap.set('n', '<leader>sd', vim.lsp.buf.hover, {})
+			vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+        end
+	}
+}
